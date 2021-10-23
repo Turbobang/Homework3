a41a2f21143a148bad5f3eac8bfc6fc8382c6f5b
LiuWeibang <1826810505@qq.com>
2021-10-23 23:07:36 +0800

    first commit

---
+++ client.bat
@@ -0,0 +1,1 @@
+ "G:\UE4\UnrealEngine\Engine\Binaries\Win64\UE4Editor.exe" "%CD%\Netshoot.uproject" 127.0.0.1 -game -log -nosteam
---
+++ server.bat
@@ -0,0 +1,1 @@
+"G:\UE4\UnrealEngine\Engine\Binaries\Win64\UE4Editor.exe" "%CD%\Netshoot.uproject" Level_01 -server -log -nosteam
